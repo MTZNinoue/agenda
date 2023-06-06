7677ea7155d916c3e00be8b4c5bd1ef9900e4303
MatheusI <matheusinoue2006@gmail.com>
2023-06-06 10:21:13 -0300

    muilirio

--- altera_agenda_exe.php
+++ altera_agenda_exe.php
@@ -2,28 +2,35 @@
     include('conexao.php');
     $id_agenda = $_POST['id_agenda'];
     $nome = $_POST['nome'];
+    $apelido = $_POST['apelido'];
+    $endereco = $_POST['endereco'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
     $telefone = $_POST['telefone'];
     $celular = $_POST['celular'];
-    $email = $_POST['email'];
-    
-   
+    $email= $_POST['email'];
 
     echo "<h1>Alteração de dados </h1>";
-    echo "<p>Usuario: $nome</p>";
-    $sql = "UPDATE usuario SET
+    echo "<p>Agenda: $nome</p>";
+    $sql = "UPDATE agenda SET
             nome='$nome',
             apelido='$apelido',
+            endereco='$endereco',
+            bairro='$bairro',
+            cidade='$cidade',
+            estado='$estado',
             telefone='$telefone',
             celular='$celular',
-            
-            WHERE id_usuario=$id_usuario";
+            email='$email'
+            WHERE id_agenda=$id_agenda";
         
-    echo $sql. "<br>";
+    //echo $sql. "<br>";
     $result = mysqli_query($con, $sql);
-    if($result)
+     if($result)
         echo "Dados alterados com sucesso!<br>";
     else
-        echo "Erro ao alterar dados: $msqli_erro($con)!";
+        echo "Erro ao alterar dados: ". mysqli_error($con)."!";
   
 ?>
 <a href="index.php">Voltar</a>
