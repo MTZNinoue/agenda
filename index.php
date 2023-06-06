fe7c544e12add3b3a6c777fdf53b3dae88b1c0f7
MatheusI <matheusinoue2006@gmail.com>
2023-06-06 07:42:07 -0300

    muilirio

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    include('criasessao.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Pagina Inicial - Projeto Cadastro IFSP</h3>
+    <?php  
+        if(!empty($_SESSION['login'])){
+            echo "<h4> Óla ". $_SESSION['login']['nome_usuario']."</h4>";
+            echo "<a href='logout.php'>Sair</a>";
+        }
+   ?>
+        <hr>
+    <ul>
+        <?php
+        if(empty($_SESSION['login'])){
+            echo "<li><a href='login.html'>Login</a></li>";
+        }
+        ?> 
+        <li><a href='cadastro_usuario.html'>Cadastrar</a></li> 
+        <li><a href='lista_usuario.php'>Listar</a></li> 
+    </ul>
+</body>
+</html>
